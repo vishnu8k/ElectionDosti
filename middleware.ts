940b79625fd711430f770117b24ce02a5f13c9ed
vishnu8k <vishnuvarthan22@gmail.com>
2026-04-30 11:35:44 +0530

    fix(deploy): correctly spoof x-forwarded headers to prevent next-intl absolute url port leakage

--- middleware.ts
+++ middleware.ts
@@ -10,6 +10,13 @@ const intlMiddleware = createMiddleware({
 });
 
 export default function middleware(req: NextRequest) {
+  req.headers.set('x-forwarded-port', '443');
+  req.headers.set('x-forwarded-proto', 'https');
+  const host = req.headers.get('x-forwarded-host') || req.headers.get('host');
+  if (host) {
+    req.headers.set('x-forwarded-host', host.split(':')[0]);
+    req.headers.set('host', host.split(':')[0]);
+  }
   req.nextUrl.port = '';
   return intlMiddleware(req);
 }
